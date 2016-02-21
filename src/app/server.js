1f4138dfd04ac61579cc20598308919136583900
Tyler Daniel <tylerdaniel@Tylers-MacBook-Air.local>
2016-02-21 11:27:54 -0700

    using userRouter

--- src/app/server.js
+++ src/app/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var config = require('./config.json');
+var userRouter = require('./user/user-router')();
+
+app.use('/user', userRouter);
 
 app.listen(config.server.port, () => {
     console.log('listening on port %s', config.server.port); 
