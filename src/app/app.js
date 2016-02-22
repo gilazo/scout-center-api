213ce29a0db4d78dd35fbb46013dbd1659873ef4
Tyler Daniel <tylerdaniel@Tylers-MacBook-Air.local>
2016-02-21 21:38:39 -0700

    allow POST /user without authentication

--- src/app/app.js
+++ src/app/app.js
@@ -7,18 +7,20 @@ var rankRouter = require('./rank/rank-router')();
 module.exports = () => {
     var app = express();
     
-    app.use((req, res, next) => {    
-        authorizationService.authorize(auth(req), authorized => {
-            if (authorized === false) {
-                return res.status(401).send();
-            }
-            
-            next(); 
-        });
+    app.use((req, res, next) => {
+        if (req.path !== '/user' && req.method !== 'POST') {
+            authorizationService.authorize(auth(req), authorized => {
+                if (authorized === false) {
+                    return res.status(401).send();
+                }                
+            });    
+        }        
+        
+        next();  
     });
 
     app.use('/user', userRouter);
     app.use('/ranks', rankRouter);
     
     return app;
-}; 
+};
--- src/test/app.spec.js
+++ src/test/app.spec.js
@@ -1,9 +1,22 @@
 var expect = require('expect.js');
 var request = require('supertest');
 var rewire = require('rewire');
+var express = require('express');
 var app = rewire('../app/app');
 
 describe('app', () => {
+    var mockUserRouter = () => {                
+        var mockApp = express();        
+        
+        mockApp.post('/', (req, res) => {
+            res.status(200).send(); 
+        });
+        
+        return mockApp;
+    };
+    
+    app.__set__('userRouter', mockUserRouter());
+    
     request = request(app());
     
     it('should return a 401 for unauthorized requests', done => {
@@ -16,4 +29,15 @@ describe('app', () => {
                 done();
             });
     });
+    
+    it('should allow a POST /user request without authorization', done => {
+        request
+            .post('/user')
+            .expect(200)
+            .end(err => {
+                if (err) return done(err);
+                
+                done(); 
+            });
+    });    
 });
